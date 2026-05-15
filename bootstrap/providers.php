9680a92c8d93b9268915582a75e4dac4f58ef7e5
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-05-15 22:27:09 +0800

    commit 2125

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\IdentityAccessServiceProvider::class,
     App\Providers\NoteRevisionServiceProvider::class,
+    App\Providers\MobileApiServiceProvider::class,
     App\Providers\HexagonalServiceProvider::class,
 ];
