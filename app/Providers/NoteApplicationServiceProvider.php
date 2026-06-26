41b35daac0cb58c9b6c82e1202ba0490f8e69a13
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-26 08:18:42 +0800

    commit 3130

--- app/Providers/NoteApplicationServiceProvider.php
+++ app/Providers/NoteApplicationServiceProvider.php
@@ -11,13 +11,19 @@ use App\Application\Note\Services\AddWorkItemErrorClassifier;
 use App\Application\Note\Services\AutoCloseNoteWhenFullyPaid;
 use App\Application\Note\Services\BuildCreateNoteRevisionSettlement;
 use App\Application\Note\Services\BuildNoteRevisionSettlement;
+use App\Application\Note\Services\CurrentRevision\CurrentRevisionRowSettlementProjector;
 use App\Application\Note\Services\FinalizePaidNoteCorrection;
 use App\Application\Note\Services\NoteCorrectionSnapshotBuilder;
 use App\Application\Note\Services\NoteHistoryProjectionService;
+use App\Application\Note\Services\NoteCurrentRevisionResolver;
+use App\Application\Note\Services\NoteOperationalStatusEvaluator;
+use App\Application\Note\Services\NoteOperationalStatusResolver;
 use App\Application\Note\Services\NoteRowSettlementSummaryBuilder;
 use App\Application\Note\Services\PersistNoteMutationTimeline;
 use App\Application\Note\Services\WorkItemFactory;
 use App\Application\Note\Services\WorkItemStatusTransitionService;
+use App\Ports\Out\Payment\CustomerRefundReaderPort;
+use App\Ports\Out\Payment\PaymentAllocationReaderPort;
 use Illuminate\Support\ServiceProvider;
 
 class NoteApplicationServiceProvider extends ServiceProvider
@@ -33,6 +39,13 @@ class NoteApplicationServiceProvider extends ServiceProvider
         $this->app->singleton(AutoCloseNoteWhenFullyPaid::class);
         $this->app->singleton(NoteCorrectionSnapshotBuilder::class);
         $this->app->singleton(NoteHistoryProjectionService::class);
+        $this->app->singleton(NoteOperationalStatusResolver::class, fn ($app) => new NoteOperationalStatusResolver(
+            $app->make(PaymentAllocationReaderPort::class),
+            $app->make(CustomerRefundReaderPort::class),
+            $app->make(NoteOperationalStatusEvaluator::class),
+            $app->make(NoteCurrentRevisionResolver::class),
+            $app->make(CurrentRevisionRowSettlementProjector::class),
+        ));
         $this->app->singleton(NoteRowSettlementSummaryBuilder::class);
         $this->app->singleton(BuildNoteRevisionSettlement::class);
         $this->app->singleton(BuildCreateNoteRevisionSettlement::class);
