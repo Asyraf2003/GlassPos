4a109f7bbb385f1be0c9d2c4cf3a3ec233a4050d
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-29 23:06:22 +0800

    commit 3436

--- app/Adapters/Out/Reporting/Queries/DashboardOperationalPerformance/StoreStockCogsPerDayQuery.php
+++ app/Adapters/Out/Reporting/Queries/DashboardOperationalPerformance/StoreStockCogsPerDayQuery.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\DB;
 
 final class StoreStockCogsPerDayQuery
 {
+    private const REVISION_REVERSE_SOURCE_TYPE = 'transaction_workspace_updated';
+
     /**
      * @return list<array{
      *   period_key:string,
@@ -41,9 +43,22 @@ final class StoreStockCogsPerDayQuery
             ->pluck('amount_rupiah', 'period_key')
             ->all();
 
+        $revisionReversedRows = DB::table('inventory_movements')
+            ->where('movement_type', 'stock_in')
+            ->where('source_type', self::REVISION_REVERSE_SOURCE_TYPE)
+            ->whereBetween('tanggal_mutasi', [$fromDate, $toDate])
+            ->selectRaw(
+                'tanggal_mutasi as period_key, ' .
+                'COALESCE(SUM(total_cost_rupiah), 0) as amount_rupiah'
+            )
+            ->groupBy('tanggal_mutasi')
+            ->pluck('amount_rupiah', 'period_key')
+            ->all();
+
         $periodKeys = array_unique([
             ...array_keys($issuedRows),
             ...array_keys($returnedRows),
+            ...array_keys($revisionReversedRows),
         ]);
 
         sort($periodKeys);
@@ -53,7 +68,8 @@ final class StoreStockCogsPerDayQuery
                 'period_key' => $periodKey,
                 'period_label' => $periodKey,
                 'amount_rupiah' => (int) ($issuedRows[$periodKey] ?? 0)
-                    - (int) ($returnedRows[$periodKey] ?? 0),
+                    - (int) ($returnedRows[$periodKey] ?? 0)
+                    - (int) ($revisionReversedRows[$periodKey] ?? 0),
             ],
             $periodKeys
         );
